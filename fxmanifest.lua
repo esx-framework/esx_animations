91064924856e9d98c3cc281e77bbae5a691387a9
Benzo <102178921+Benzo00@users.noreply.github.com>
2023-01-18 14:38:57 +0100

    feat - new repos

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'ESX Animations'
+lua54 'yes'
+
+version '1.0'
+legacyversion '1.9.1'
+
+client_scripts {
+	'@es_extended/imports.lua',
+	'config.lua',
+	'client/main.lua'
+}
+
+dependency 'es_extended'
